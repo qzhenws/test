d681f332478b52880caff34cef227f12dff16dc5
zhh <zhh.qiu@maxmac.com.cn>
2022-12-07 14:12:54 +0800

    断电测试

---
+++ USB2XXX_www/USB2XXX_LIN_Test/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace USB2XXX_LIN_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lvl = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ch = new System.Windows.Forms.ComboBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开LIN设备";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(41, 87);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 18);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "设备未打开";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lvl
+            // 
+            this.lvl.FormattingEnabled = true;
+            this.lvl.Items.AddRange(new object[] {
+            "关闭",
+            "LVL 1",
+            "LVL 2",
+            "LVL 3",
+            "LVL 4",
+            "LVL 5",
+            "LVL 6",
+            "LVL 7",
+            "LVL 8",
+            "LVL 9",
+            "LVL 10",
+            "LVL INIT",
+            "LVL ERR"});
+            this.lvl.Location = new System.Drawing.Point(41, 196);
+            this.lvl.Margin = new System.Windows.Forms.Padding(4);
+            this.lvl.Name = "lvl";
+            this.lvl.Size = new System.Drawing.Size(160, 23);
+            this.lvl.TabIndex = 2;
+            this.lvl.Text = "select";
+            this.lvl.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "浓度选择";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "通道选择";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ch
+            // 
+            this.ch.FormattingEnabled = true;
+            this.ch.Items.AddRange(new object[] {
+            "INI 0",
+            "CH 1",
+            "CH 2",
+            "CH 3"});
+            this.ch.Location = new System.Drawing.Point(236, 196);
+            this.ch.Margin = new System.Windows.Forms.Padding(4);
+            this.ch.Name = "ch";
+            this.ch.Size = new System.Drawing.Size(160, 23);
+            this.ch.TabIndex = 4;
+            this.ch.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button_start
+            // 
+            this.button_start.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_start.Location = new System.Drawing.Point(343, 26);
+            this.button_start.Margin = new System.Windows.Forms.Padding(4);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(157, 40);
+            this.button_start.TabIndex = 6;
+            this.button_start.Text = "开始";
+            this.button_start.UseMnemonic = false;
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 240;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(680, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(355, 411);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "大众LIN测试";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox lvl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ch;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ USB2XXX_www/USB2XXX_LIN_Test/Form1.cs
@@ -0,0 +1,360 @@
+﻿
+
+#define LIN_MASTER
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using USB2XXX;
+
+namespace USB2XXX_LIN_Test
+{
+
+    public partial class Form1 : Form
+    {
+        bool MSGStringFlag = true;
+        String MSGString;
+        Int32[] DevHandles = new Int32[20];
+        Int32 DevHandle = 0;
+        Byte LINIndex = 0;
+        bool state;
+        bool Initstate = false;
+        Int32 DevNum, ret;
+        static Byte tickCnt = 0;
+        byte[] RcvDataBuffer = new byte[8];
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                usb_device.DEVICE_INFO DevInfo = new usb_device.DEVICE_INFO();
+                //扫描查找设备
+                DevNum = usb_device.USB_ScanDevice(DevHandles);
+                if (DevNum <= 0)
+                {
+                    Console.WriteLine("No device connected!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Have {0} device connected!", DevNum);
+                }
+                DevHandle = DevHandles[0];
+                //打开设备
+                state = usb_device.USB_OpenDevice(DevHandle);
+                if (!state)
+                {
+                    Console.WriteLine("Open device error!");
+                    textBox1.Text = "Open Fail...";
+                    textBox1.BackColor = Color.Red;
+                    USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 0);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Open device success!");
+                    textBox1.Text = "Opend!!!";
+                    textBox1.BackColor = Color.Green;
+                    USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 1); //12v
+                }
+                //获取固件信息
+                StringBuilder FuncStr = new StringBuilder(256);
+                state = usb_device.DEV_GetDeviceInfo(DevHandle, ref DevInfo, FuncStr);
+                if (!state)
+                {
+                    Console.WriteLine("Get device infomation error!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Firmware Info:");
+                    Console.WriteLine("    Name:" + Encoding.Default.GetString(DevInfo.FirmwareName));
+                    Console.WriteLine("    Build Date:" + Encoding.Default.GetString(DevInfo.BuildDate));
+                    Console.WriteLine("    Firmware Version:v{0}.{1}.{2}", (DevInfo.FirmwareVersion >> 24) & 0xFF, (DevInfo.FirmwareVersion >> 16) & 0xFF, DevInfo.FirmwareVersion & 0xFFFF);
+                    Console.WriteLine("    Hardware Version:v{0}.{1}.{2}", (DevInfo.HardwareVersion >> 24) & 0xFF, (DevInfo.HardwareVersion >> 16) & 0xFF, DevInfo.HardwareVersion & 0xFFFF);
+                    Console.WriteLine("    Functions:" + DevInfo.Functions.ToString("X8"));
+                    Console.WriteLine("    Functions String:" + FuncStr);
+                }
+                //初始化配置LIN
+                USB2LIN.LIN_CONFIG LINConfig = new USB2LIN.LIN_CONFIG();
+                LINConfig.BaudRate = 19200;
+                LINConfig.BreakBits = USB2LIN.LIN_BREAK_BITS_11;
+                LINConfig.CheckMode = USB2LIN.LIN_CHECK_MODE_EXT;
+#if LIN_MASTER
+                LINConfig.MasterMode = USB2LIN.LIN_MASTER;
+#else
+            LINConfig.MasterMode = USB2LIN.LIN_SLAVE;
+#endif
+                ret = USB2LIN.LIN_Init(DevHandle, LINIndex, ref LINConfig);
+                if (ret != USB2LIN.LIN_SUCCESS)
+                {
+                    Console.WriteLine("Config LIN failed!");
+                    textBox1.Text = "Open LIN failed!";
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Config LIN Success!");
+                    textBox1.Text = "Open LIN Success!";
+                }
+#if LIN_MASTER
+#endif
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (true == Initstate)
+                {
+                    Initstate = false;
+                    button_start.Text = "开始";
+                    button_start.ForeColor = System.Drawing.SystemColors.ControlText;
+                    USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 0);//设置输出电压（0=0V,1=12V,2=5V）
+
+                }
+                else if (false == Initstate)
+                {
+                    Initstate = true;
+                    button_start.Text = "停止";
+                    button_start.ForeColor = Color.DarkRed;
+                    USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 1);//设置输出电压（0=0V,1=12V,2=5V）
+                    timer1.Stop();
+                    timer1.Start();
+                    tickCnt = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int[] bytArray = new int[8];
+                tickCnt++;
+                if (true == Initstate)
+                {
+
+                    if (0u == tickCnt % 2)
+                    {
+                        if (20 > tickCnt)
+                        {
+                            Send_data(new Byte[8] { 0xff, 0xff, 0xff, 0x7F, 0x00, 0x00, 0x14, 0x00 });
+                        }
+                        else 
+                        {
+                            Send_data(new Byte[8] { 0xff, 0xff, 0xff, 0x7F, 0x00, 0x00, 0x18, 0x00 });
+                        }
+                    }
+                    else
+                    {
+                        RcvDataBuffer = Receive_data();
+
+                        if (10 < tickCnt)
+                        {
+                            if (0x63 == RcvDataBuffer[1])
+                            {
+                                Initstate = false;
+                                button_start.Text = "开始";
+                                button_start.ForeColor = System.Drawing.SystemColors.ControlText;
+                                MessageBox.Show("LIN message reset happenend!!!!");
+                            }
+                        }
+                    }
+                    if (40 == tickCnt)
+                    {
+                        USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 0);//设置输出电压（0=0V,1=12V,2=5V）
+                    }
+                    if (44 <= tickCnt)
+                    {
+                        tickCnt = 0;
+                        USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 1);//设置输出电压（0=0V,1=12V,2=5V）
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        //若出现USB断开连接，使用该函数进行重连设备
+        public bool LIN_ReConnect()
+        {
+            try
+            {
+                DevNum = usb_device.USB_ScanDevice(DevHandles);// 扫描查找设备
+                if (DevNum <= 0)
+                {
+                    textBox1.Text = "没有连接驱动器！";//
+                    return false;
+                }
+                DevHandle = DevHandles[0];
+                bool state = usb_device.USB_OpenDevice(DevHandle);//打开设备
+                if (state)
+                {
+                    int ret;
+                    ret = USB2LIN_EX.LIN_EX_Init(DevHandle, LINIndex, 19200, 1);//初始化为主机
+                    if (ret == USB2LIN_EX.LIN_EX_SUCCESS)
+                    {
+                        textBox1.Text = "驱动器连接成功！";//
+                        USB2LIN_EX.LIN_EX_CtrlPowerOut(DevHandle, LINIndex, 0);//设置输出电压（0=0V,1=12V,2=5V）
+                        return true;
+                    }
+                }
+                textBox1.Text = "驱动器连接失败！";//
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return false;
+        }
+
+        public void Send_data(byte[] byteArray)//发送数据
+        {
+            try
+            {
+                ret = USB2LIN_EX.LIN_EX_MasterWrite(DevHandle, LINIndex, (byte)0x2C, byteArray, (byte)byteArray.Length, USB2LIN_EX.LIN_EX_CHECK_EXT);//发送耗时10ms
+                if (ret != USB2LIN_EX.LIN_EX_SUCCESS)
+                {
+                    LIN_ReConnect();
+                    MSGString += DateTime.Now.ToString("HH:mm:ss.fff") + " " + "ID[3D] " + "LIN_ReConnect!" + "\r\n";
+                }
+                if (MSGStringFlag == true)
+                {
+                    MSGString += DateTime.Now.ToString("mm:ss.fff") + " " + "[2C] " +
+                                        (byteArray[0]).ToString("X2") + " " + (byteArray[1]).ToString("X2") + " " +
+                                        (byteArray[2]).ToString("X2") + " " + (byteArray[3]).ToString("X2") + " " +
+                                        (byteArray[4]).ToString("X2") + " " + (byteArray[5]).ToString("X2") + " " +
+                                        (byteArray[6]).ToString("X2") + " " + (byteArray[7]).ToString("X2") + "\r\n";
+
+                    textBox2.AppendText(DateTime.Now.ToString("mm:ss.fff") + " " + "[2C] " +
+                                        (byteArray[0]).ToString("X2") + " " + (byteArray[1]).ToString("X2") + " " +
+                                        (byteArray[2]).ToString("X2") + " " + (byteArray[3]).ToString("X2") + " " +
+                                        (byteArray[4]).ToString("X2") + " " + (byteArray[5]).ToString("X2") + " " +
+                                        (byteArray[6]).ToString("X2") + " " + (byteArray[7]).ToString("X2") + "\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        public byte[] Receive_data()//接收数据
+        {
+            
+            byte[] DataBuffer = new byte[8] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            try
+            { 
+                ret = USB2LIN_EX.LIN_EX_MasterRead(DevHandle, LINIndex, (byte)0x08, DataBuffer);
+                if (ret <= USB2LIN_EX.LIN_EX_SUCCESS)//判断读取结果
+                {
+                    if (ret < 0)//LIN通讯错误
+                    {
+                        LIN_ReConnect();//重新连接
+                    }
+                    else
+                    {
+
+                        textBox2.AppendText(DateTime.Now.ToString("mm:ss.fff") + " " + "[08] " + "\r\n");
+                  
+
+
+                    }
+                }
+                else
+                {//主机发送数据成功后，也会接收到发送出去的数据，通过接收回来的数据跟发送出去的数据对比，可以判断发送数据的时候，数据是否被冲突
+                    if (MSGStringFlag == true)
+                    {
+                        MSGString += DateTime.Now.ToString("mm:ss.fff") + " " + "ID[3D] " +
+                                        (DataBuffer[0]).ToString("X2") + " " + (DataBuffer[1]).ToString("X2") + " " +
+                                        (DataBuffer[2]).ToString("X2") + " " + (DataBuffer[3]).ToString("X2") + " " +
+                                        (DataBuffer[4]).ToString("X2") + " " + (DataBuffer[5]).ToString("X2") + " " +
+                                        (DataBuffer[6]).ToString("X2") + " " + (DataBuffer[7]).ToString("X2") + "\r\n";
+
+                        textBox2.AppendText(DateTime.Now.ToString("mm:ss.fff") + " " + "[08] " +
+                                        (DataBuffer[0]).ToString("X2") + " " + (DataBuffer[1]).ToString("X2") + " " +
+                                        (DataBuffer[2]).ToString("X2") + " " + (DataBuffer[3]).ToString("X2") + " " +
+                                        (DataBuffer[4]).ToString("X2") + " " + (DataBuffer[5]).ToString("X2") + " " +
+                                        (DataBuffer[6]).ToString("X2") + " " + (DataBuffer[7]).ToString("X2") + "\r\n");
+                    }
+                }
+                return DataBuffer;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return DataBuffer;
+            }
+        }
+    }
+}
+    
